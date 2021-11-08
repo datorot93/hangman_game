d3ff2f9ceefcfdae07cfb10789ebddde9240d59e
datorot93 <herrion1@hotmail.com>
2021-11-07 19:17:14 -0500

    Some error handlings added to the project

--- hangman_game.py
+++ hangman_game.py
@@ -4,21 +4,26 @@ from hangman_art import HANGMANPICS
 # Utilities
 import os
 import random
+import re
 
 
 
 def read_file():
-    words = []
-
-    with open('./data/data.txt', 'r', encoding='utf_8') as file:
-
-        words = [word for word in file if word != '\n']
-    
-    return words
+    try:
+        words = []
+        with open('./files/data.txt', 'r', encoding='utf_8') as file:
+            words = [word for word in file if word != '\n']
+    except FileNotFoundError:
+        print('There is not exists a data file with the words')
+        exit()
+    return words    
 
 def choose_word(words):
-    word = random.choice(words)
-    
+    try:
+        word = random.choice(words)
+    except IndexError:
+        print('The data file does not contain any word. Please ensure that it is not empty.')
+        exit()
     return word
 
 def make_dict(word):
@@ -40,6 +45,11 @@ def set_letters(word_dict, letter):
 
     return word_dict
 
+def is_letter(letter):
+    if re.search('[a-zA-Z]', letter):
+        return True
+    return False
+
 def run():
     errors = 0
 
@@ -49,13 +59,20 @@ def run():
     os.system('clear')  
     print_word(word_dict, word)
 
-    while False in list(word_dict.values()) or errors < len(HANGMANPICS):
+    while False in list(word_dict.values()) and errors < len(HANGMANPICS):
         print()
+        print(word_dict)
         print('Errors: {}'.format(errors))
         letter = input('Please enter a letter: ')
+        if not is_letter(letter) or len(letter) > 1:
+            os.system('clear')  
+            print_word(word_dict, word)
+            print('Your input have to be ONE letter, it can not be a number or a text with more than 1 character')
+            continue
+
         word_dict = set_letters(word_dict, letter)
         if letter not in word:
-            os.system('clear')     
+            os.system('clear')
             print(HANGMANPICS[errors])
             print_word(word_dict, word)
             errors+=1
@@ -65,7 +82,12 @@ def run():
                 print(HANGMANPICS[errors])
             print_word(word_dict, word)
 
+    if errors < len(HANGMANPICS):
+        os.system('clear')
+        print('#' * 20)
+        print('CONGRATULATIONS, YOU WON!!!')
+        print('#' * 20)
+
 
 if __name__=='__main__':
-    print(HANGMANPICS[0])
     run()
